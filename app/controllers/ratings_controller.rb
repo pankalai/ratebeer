33acf7fdf08497260e518498f65cc8f716ea68f6
Anssi Pänkäläinen <anssi.pankalainen@gmail.com>
2023-12-04 22:11:15 +0200

    week 7

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -6,13 +6,13 @@ class RatingsController < ApplicationController
     @ratings = Rating.all
 
     # @beers_and_ratings = Beer.includes(:ratings).sort_by{|b| [b.average_rating, b.ratings.count]}
-    @beers_and_ratings = Rails.cache.fetch("beers and ratings") { Beer.includes(ratings: { user: [] }).sort_by{ |b| [b.average_rating, b.ratings.count] } }
-    @top_breweries = Rails.cache.fetch("brewery top 3") { Brewery.top(3) }
-    @top_beers = Rails.cache.fetch("beer top 3") { Beer.top(3) }
-    @top_styles = Rails.cache.fetch("style top 3") { Style.top(3) }
-    @active_users = Rails.cache.fetch("user top 3") { User.top(3) }
+    @beers_and_ratings = Rails.cache.fetch("beers and ratings", expires_in: 5.minutes) { Beer.includes(ratings: { user: [] }).sort_by{ |b| [b.average_rating, b.ratings.count] } }
+    @top_breweries = Rails.cache.fetch("brewery top 3", expires_in: 5.minutes) { Brewery.top(3) }
+    @top_beers = Rails.cache.fetch("beer top 3", expires_in: 5.minutes) { Beer.top(3) }
+    @top_styles = Rails.cache.fetch("style top 3", expires_in: 5.minutes) { Style.top(3) }
+    @active_users = Rails.cache.fetch("user top 3", expires_in: 5.minutes) { User.top(3) }
 
-    CacheUpdate.perform_async
+    #CacheUpdate.perform_async
   end
 
   def new
--- app/jobs/cache_update.rb
+++ app/jobs/cache_update.rb
@@ -2,18 +2,18 @@ class CacheUpdate
   include SuckerPunch::Job
 
   def perform
-    Rails.cache.write("beers and ratings", Beer.includes(ratings: { user: [] }).sort_by{ |b| [b.average_rating, b.ratings.count] }, expires_in: 10.minutes) if cache_does_not_contain_data("beers and ratings")
+    time = 5.minutes
+    Rails.cache.write("beers and ratings", Beer.includes(ratings: { user: [] }).sort_by{ |b| [b.average_rating, b.ratings.count] }, expires_in: time) #if cache_does_not_contain_data("beers and ratings")
 
-    Rails.cache.write("brewery top 3", Brewery.top(3), expires_in: 10.minutes) if cache_does_not_contain_data("brewery top 3")
-    Rails.cache.write("beer top 3", Beer.top(3), expires_in: 10.minutes) if cache_does_not_contain_data("beer top 3")
-    Rails.cache.write("style top 3", Style.top(3), expires_in: 10.minutes) if cache_does_not_contain_data("style top 3")
-    Rails.cache.write("user top 3", User.top(3), expires_in: 10.minutes) if cache_does_not_contain_data("user top 3")
-    # Rails.cache.write("beers and ratings", Beer.includes(:ratings).sort_by{|b| [b.average_rating, b.ratings.count]}, expires_in: 1.minutes) if cache_does_not_contain_data("beers and ratings")
+    Rails.cache.write("brewery top 3", Brewery.top(3), expires_in: time) #if cache_does_not_contain_data("brewery top 3")
+    Rails.cache.write("beer top 3", Beer.top(3), expires_in: time) #if cache_does_not_contain_data("beer top 3")
+    Rails.cache.write("style top 3", Style.top(3), expires_in: time) #if cache_does_not_contain_data("style top 3")
+    Rails.cache.write("user top 3", User.top(3), expires_in: time) #if cache_does_not_contain_data("user top 3")
 
-    # CacheUpdate.perform_in(10.minutes)
+    CacheUpdate.perform_in(time)
   end
 
-  def cache_does_not_contain_data(name)
-    !Rails.cache.exist?(name)
-  end
+  # def cache_does_not_contain_data(name)
+  #   return !Rails.cache.exist?(name) 
+  # end
 end
