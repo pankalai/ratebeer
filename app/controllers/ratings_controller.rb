988e0f19315f30c616f258f36ca38e7e596773a3
Anssi Pänkäläinen <anssi.pankalainen@gmail.com>
2023-12-04 22:50:27 +0200

    week 7

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -6,7 +6,7 @@ class RatingsController < ApplicationController
     @ratings = Rating.all
 
     # @beers_and_ratings = Beer.includes(:ratings).sort_by{|b| [b.average_rating, b.ratings.count]}
-    @beers_and_ratings = Rails.cache.fetch("beers and ratings", expires_in: 5.minutes) { Beer.includes(ratings: { user: [] }).sort_by{ |b| [b.average_rating, b.ratings.count] } }
+    @beers_and_ratings = Rails.cache.fetch("beers and ratings", expires_in: 5.minutes) { Beer.includes(ratings: { user: [] }).sort_by{ |b| [b.ratings.count] }.reverse.take(50) }
     @top_breweries = Rails.cache.fetch("brewery top 3", expires_in: 5.minutes) { Brewery.top(3) }
     @top_beers = Rails.cache.fetch("beer top 3", expires_in: 5.minutes) { Beer.top(3) }
     @top_styles = Rails.cache.fetch("style top 3", expires_in: 5.minutes) { Style.top(3) }
